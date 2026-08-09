651cc10019a020bef5fd652fce14bd2e6d8a1e53
Dev Sharma <devsharma2057@gmail.com>
2026-08-09 21:06:51 +0530

    05: Add Social Authentication

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import LogoutButton from "../modules/dashboard/ui/view/logout-button"
+
+const Dashboard = async () => {
+  return (
+    <div>
+        <h2>Dashboard</h2>
+        <LogoutButton />
+    </div>
+  )
+}
+
+export default Dashboard
